77ffeb33d2c18e76c4f5e1757fbf5b7aed388299
Tenkaklet <tenkaklet@gmail.com>
2015-08-04 00:02:40 +0200

    introduced classes to btns

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-$(document).ready(function(){
-	$('#add').click(function(){		
+$(document).ready(function() {
+	$('#add').click(function() {		
 		if ($('#item').val() !== "") {
 			$('#list').append('<li class="thing">' + $('#item').val() + '</li>');
 		}
 	});
-	$(document).keydown(function(){
+
+	$(document).keydown(function() {
 		if (event.which === 13) {
 			$('#add').trigger('click');
 		}
 	});
-	$('#remove-completed').click(function(){
+
+	$('#remove-completed').click(function() {
 		$('#list li.completed').remove();
 	});
+
+	$('.single-remove').click(function() {
+		$(this).parent().remove();
+	});
+
+	$('#done').click(function(){
+		$('#list li').addClass('completed');
+	});
+
 });
