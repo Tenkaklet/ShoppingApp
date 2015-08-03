9d579706698444ec88d2ea7243f4664a4421c733
Tenkaklet <tenkaklet@gmail.com>
2015-08-03 11:13:07 +0200

    twerking the works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 	$('#add').click(function(){
-		$('#list').append('<li>' + $('#item').val() + '</li>');
-		$('#item').val('');
+		$('#list').append('<li class="thing">' + $('#item').val() + '</li>');
+		$('#item').val();
+	});
+	$(document).keyup(function(keypressed) {
+		if (keypressed.keyCode === 13) {
+			$('#add').trigger('click');
+		}
+	});
+	$('#delete').click(function(){
+		$('#list li:last-child').remove();
 	});
 });
